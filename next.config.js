bf9c43ac3de0828a1bf71526288130b0565befef
Leonardo de Oliveira <leonardooliveira.sl@gmail.com>
2023-03-22 10:35:28 -0300

    feat: Cria nextjs.yml

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  assetPrefix: isProd ? "/oleonardo.github.io/" : "",
   images: {
     unoptimized: true,
   },
